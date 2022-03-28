32adc0c5dba43d96352af0c99d7cd88d39f3b9f8
2804271734@qq.com <2804271734@qq.com>
2022-03-28 01:01:06 -0700

    ruby -1.0.0

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '2c68ef04b53e222a5a144c4d2a42e7b7f79a6ce9daade4e740c2413a34ac0079aa8895c071b9afbe60c084cb0e781f50e5decfaeb0fadab12da12843b3658a5c'
